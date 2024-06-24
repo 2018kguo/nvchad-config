03ef59df01f99a787ed8206d337a51443b254fe8
Kevin Guo <kev.guo123@gmail.com>
2024-06-23 21:13:40 -0400

    diagnostics mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,3 +15,8 @@ map("i", "<C-l>", function()
     vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
 end, { desc = "Accept copilot suggestion" , noremap = true, silent = true, expr = true, nowait = true, replace_keycodes = true})
 
+-- Key mappings for LSP diagnostics
+map('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = "Open diagnostic float", noremap = true, silent = true })
+map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { desc = "Go to previous diagnostic", noremap = true, silent = true })
+map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { desc = "Go to next diagnostic", noremap = true, silent = true })
+map('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { desc = "Set diagnostics to location list", noremap = true, silent = true })
